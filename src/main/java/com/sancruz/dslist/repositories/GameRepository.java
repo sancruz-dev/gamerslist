364e84ce2d453072b7d0646317484fc754d1f4eb
sancruz-dev <sancruz.dev@gmail.com>
2023-05-10 13:37:54 -0300

    Game Repository, Service, Controller

---
+++ src/main/java/com/sancruz/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.sancruz.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sancruz.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+  
+}
